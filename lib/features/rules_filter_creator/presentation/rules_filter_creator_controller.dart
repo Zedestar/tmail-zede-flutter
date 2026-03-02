554c751bb867af44d0087a93eb265a1f663a6a04
dab246 <tdvu@linagora.com>
2026-03-02 16:36:34 +0700

    TF-4351 Handle edit rule with Label message

--- lib/features/rules_filter_creator/presentation/rules_filter_creator_controller.dart
+++ lib/features/rules_filter_creator/presentation/rules_filter_creator_controller.dart
@@ -286,6 +286,10 @@ class RulesFilterCreatorController extends BaseMailboxController
 
         _newRuleName = _currentTMailRule!.name;
         _setValueInputField(inputRuleNameController, _newRuleName ?? '');
+
+        if (_isLabelAvailable) {
+          setUpLabelMessageWhenEditRule(_currentTMailRule!);
+        }
         break;
     }
     inputRuleNameFocusNode.requestFocus();
