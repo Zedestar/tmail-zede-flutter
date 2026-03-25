stesttest/tmail-flutter

Add Scribe mobile
Same features than Scribe desktop but as a full screen modal instead of  a context menu.

Reuse menu and submenu components.
Reuse suggestion components.
Reuse 99% of Scribe desktop style.

scribe/lib/scribe/ai/presentation/utils/modal/ai_scribe_modal_manager.dart
@@ -1,4 +1,5 @@
 import 'package:core/presentation/resources/image_paths.dart';
+import 'package:core/utils/platform_info.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:scribe/scribe.dart';
@@ -16,21 +17,31 @@ class AiScribeModalManager {
     ModalPlacement? preferredPlacement,
     ModalCrossAxisAlignment crossAxisAlignment = ModalCrossAxisAlignment.center,
   }) async {
-    final PopupSubmenuController submenuController = PopupSubmenuController();
+    final AIAction? aiAction;
 
-    final aiAction = await Get.dialog<AIAction>(
-      AiScribeModalWidget(
+    if (PlatformInfo.isMobile) {
+      aiAction = await showMobileAIScribeMenuModal(
         imagePaths: imagePaths,
-        content: content,
         availableCategories: availableCategories,
-        buttonPosition: buttonPosition,
-        buttonSize: buttonSize,
-        preferredPlacement: preferredPlacement,
-        crossAxisAlignment: crossAxisAlignment,
-        submenuController: submenuController,
-      ),
-      barrierColor: AIScribeColors.dialogBarrier,
-    ).whenComplete(submenuController.dispose);
+        content: content,
+      );
+    } else {
+      final PopupSubmenuController submenuController = PopupSubmenuController();
+
+      aiAction = await Get.dialog<AIAction>(
+        AiScribeModalWidget(
+          imagePaths: imagePaths,
+          content: content,
+          availableCategories: availableCategories,
+          buttonPosition: buttonPosition,
+          buttonSize: buttonSize,
+          preferredPlacement: preferredPlacement,
+          crossAxisAlignment: crossAxisAlignment,
+          submenuController: submenuController,
+        ),
+        barrierColor: AIScribeColors.dialogBarrier,
+      ).whenComplete(submenuController.dispose);
+    }
 
     if (aiAction != null) {
       await showAIScribeSuggestionModal(
@@ -56,18 +67,63 @@ class AiScribeModalManager {
     ModalPlacement? preferredPlacement,
     ModalCrossAxisAlignment crossAxisAlignment = ModalCrossAxisAlignment.center,
   }) async {
-    await Get.dialog<String?>(
-      AiScribeSuggestionWidget(
+    if (PlatformInfo.isMobile) {
+      await showMobileAIScribeSuggestionModal(
         aiAction: aiAction,
         imagePaths: imagePaths,
         content: content,
-        buttonPosition: buttonPosition,
-        buttonSize: buttonSize,
-        preferredPlacement: preferredPlacement,
-        crossAxisAlignment: crossAxisAlignment,
         onSelectAiScribeSuggestionAction: onSelectAiScribeSuggestionAction,
+      );
+    } else {
+      await Get.dialog<String?>(
+        AiScribeSuggestionWidget(
+          aiAction: aiAction,
+          imagePaths: imagePaths,
+          content: content,
+          buttonPosition: buttonPosition,
+          buttonSize: buttonSize,
+          preferredPlacement: preferredPlacement,
+          crossAxisAlignment: crossAxisAlignment,
+          onSelectAiScribeSuggestionAction: onSelectAiScribeSuggestionAction,
+        ),
+        barrierColor: AIScribeColors.dialogBarrier,
+      );
+    }
+  }
+
+  static Future<AIAction?> showMobileAIScribeMenuModal({
+    required ImagePaths imagePaths,
+    required List<AIScribeMenuCategory> availableCategories,
+    String? content,
+  }) async {
+    return await showModalBottomSheet<AIAction>(
+      context: Get.context!,
+      isScrollControlled: true,
+      useSafeArea: true,
+      builder: (context) => AiScribeMobileActionsBottomSheet(
+        imagePaths: imagePaths,
+        availableCategories: availableCategories,
       ),
-      barrierColor: AIScribeColors.dialogBarrier,
+    );
+  }
+
+  static Future<void> showMobileAIScribeSuggestionModal({
+    required AIAction aiAction,
+    required ImagePaths imagePaths,
+    required OnSelectAiScribeSuggestionAction onSelectAiScribeSuggestionAction,
+    String? content,
+  }) async {
+    await showModalBottomSheet(
+      context: Get.context!,
+      isScrollControlled: true,
+      useSafeArea: true,
+      isDismissible: true,
+      builder: (context) => AiScribeMobileSuggestionBottomSheet(
+        aiAction: aiAction,
+        imagePaths: imagePaths,
+        content: content,
+        onSelectAction: onSelectAiScribeSuggestionAction,
+      ),
     );
   }
 }

Zedestar/tmail-zede-flutter

scribe/lib/scribe/ai/presentation/utils/modal/ai_scribe_modal_manager.dart
@@ -1,4 +1,5 @@
 import 'package:core/presentation/resources/image_paths.dart';
+import 'package:core/utils/platform_info.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:scribe/scribe.dart';
@@ -16,21 +17,30 @@ class AiScribeModalManager {
     ModalPlacement? preferredPlacement,
     ModalCrossAxisAlignment crossAxisAlignment = ModalCrossAxisAlignment.center,
   }) async {
-    final ContextSubmenuController submenuController = ContextSubmenuController();
+    final AIAction? aiAction;
 
-    final aiAction = await Get.dialog<AIAction>(
-      AiScribeModalWidget(
+    if (PlatformInfo.isMobile) {
+      aiAction = await showMobileAIScribeMenuModal(
         imagePaths: imagePaths,
-        content: content,
         availableCategories: availableCategories,
-        buttonPosition: buttonPosition,
-        buttonSize: buttonSize,
-        preferredPlacement: preferredPlacement,
-        crossAxisAlignment: crossAxisAlignment,
-        submenuController: submenuController,
-      ),
-      barrierColor: AIScribeColors.dialogBarrier,
-    ).whenComplete(submenuController.dispose);
+      );
+    } else {
+      final ContextSubmenuController submenuController = ContextSubmenuController();
+
+      aiAction = await Get.dialog<AIAction>(
+        AiScribeModalWidget(
+          imagePaths: imagePaths,
+          content: content,
+          availableCategories: availableCategories,
+          buttonPosition: buttonPosition,
+          buttonSize: buttonSize,
+          preferredPlacement: preferredPlacement,
+          crossAxisAlignment: crossAxisAlignment,
+          submenuController: submenuController,
+        ),
+        barrierColor: AIScribeColors.dialogBarrier,
+      ).whenComplete(submenuController.dispose);
+    }
 
     if (aiAction != null) {
       await showAIScribeSuggestionModal(
@@ -56,18 +66,70 @@ class AiScribeModalManager {
     ModalPlacement? preferredPlacement,
     ModalCrossAxisAlignment crossAxisAlignment = ModalCrossAxisAlignment.center,
   }) async {
-    await Get.dialog<String?>(
-      AiScribeSuggestionWidget(
+    if (PlatformInfo.isMobile) {
+      await showMobileAIScribeSuggestionModal(
         aiAction: aiAction,
         imagePaths: imagePaths,
         content: content,
-        buttonPosition: buttonPosition,
-        buttonSize: buttonSize,
-        preferredPlacement: preferredPlacement,
-        crossAxisAlignment: crossAxisAlignment,
         onSelectAiScribeSuggestionAction: onSelectAiScribeSuggestionAction,
+      );
+    } else {
+      await Get.dialog<String?>(
+        AiScribeSuggestionWidget(
+          aiAction: aiAction,
+          imagePaths: imagePaths,
+          content: content,
+          buttonPosition: buttonPosition,
+          buttonSize: buttonSize,
+          preferredPlacement: preferredPlacement,
+          crossAxisAlignment: crossAxisAlignment,
+          onSelectAiScribeSuggestionAction: onSelectAiScribeSuggestionAction,
+        ),
+        barrierColor: AIScribeColors.dialogBarrier,
+      );
+    }
+  }
+
+  static Future<AIAction?> showMobileAIScribeMenuModal({
+    required ImagePaths imagePaths,
+    required List<AIScribeMenuCategory> availableCategories,
+  }) async {
+    final context = Get.context;
+
+    if (context == null) return null;
+
+    return await showModalBottomSheet<AIAction>(
+      context: context,
+      isScrollControlled: true,
+      useSafeArea: true,
+      builder: (context) => AiScribeMobileActionsBottomSheet(
+        imagePaths: imagePaths,
+        availableCategories: availableCategories,
       ),
-      barrierColor: AIScribeColors.dialogBarrier,
+    );
+  }
+
+  static Future<void> showMobileAIScribeSuggestionModal({
+    required AIAction aiAction,
+    required ImagePaths imagePaths,
+    required OnSelectAiScribeSuggestionAction onSelectAiScribeSuggestionAction,
+    String? content,
+  }) async {
+    final context = Get.context;
+
+    if (context == null) return;
+
+    await showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      useSafeArea: true,
+      isDismissible: true,
+      builder: (context) => AiScribeMobileSuggestionBottomSheet(
+        aiAction: aiAction,
+        imagePaths: imagePaths,
+        content: content,
+        onSelectAction: onSelectAiScribeSuggestionAction,
+      ),
     );
   }
 }